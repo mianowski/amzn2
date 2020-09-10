45b3af1e5346592da9f3c17912c94e1e004dff5c
= <grzegorz@mianowscy-w530.localdomain>
2020-09-10 10:11:12 +0200

    Project wireframe and basic models

---
+++ shop/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price_in_dollars = models.DecimalField(
+        decimal_places=2,
+        max_digits=6)
+    barcode = models.CharField(
+        max_length=13,
+        help_text="Product Barcode (ISBN, UPC ...)"
+    )
+    description = models.TextField(help_text="Enter Product Description")
+
+    def __str__():
+        return "%s %s" % (self.barcode, self.name)
+
+
+class Order(models.Model):
+
+    items = models.ManyToManyField(Product, through='OrderItem')
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+    )
+    quantity = models.PositiveIntegerField(default=1)
